431d993ec6e7f9704a4d1d2a52a96ae69564045d
Luca Tenuta <luca.tenuta@gmail.com>
2022-09-15 09:55:19 -0400

    added MemberRemove
    -fixed issue that user remained in database after leaving while waiting for verification.

--- extensions/auto_role.py
+++ extensions/auto_role.py
@@ -1,19 +1,40 @@
-from loguru import logger
-from naff import (listen, Extension, slash_command, InteractionContext)
-from naff.api.events import MemberAdd, MemberUpdate
+import logging
+from datetime import datetime
+from beanie import Document
+from naff import (listen, Extension, logger_name)
+from naff.api.events import MemberRemove, MemberUpdate
 from naff.models.naff.converters import RoleConverter
 
+
+logger = logging.getLogger(logger_name)
+class VerificationQueue(Document):
+    discord_id: int
+    discord_name: str
+    gamertag: str
+    status: str
+    reason: str
+    updated: datetime
+    discord_thread: int
+
+    class Collection:
+        name = "verification_queue"
 class AutoRoles(Extension):
 
     # Once member accepts screening they are assigned the role
     @listen()
     async def on_member_screening(self, event: MemberUpdate):
-        if event.before.pending:
-            if event.after.pending is False:
-                await event.after.add_role(await RoleConverter().convert(event, "New Member"), "Accepted Rules Screening.")
-        else:
-            pass
+        if event.before.pending and event.after.pending is False:
+            await event.after.add_role(await RoleConverter().convert(event, "New Member"), "Accepted Rules Screening.")
+    
+    # TODO:  if someone leaves check verification queue and remove if found.
+    @listen()
+    async def on_member_leave(self, event: MemberRemove):
+        player_in_queue = VerificationQueue.find(VerificationQueue.discord_id == event.member.id)
+
+        if player_in_queue is not None:
+            player_in_queue.delete()
 
 
 def setup(bot):
     AutoRoles(bot)
+    bot.add_model(VerificationQueue)

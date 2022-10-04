14e97425d2afaad7c4114e99254b44b88deea0f9
Luca Tenuta <luca.tenuta@gmail.com>
2022-10-04 08:35:57 -0400

    fixed issue deleting thread and member from db

--- extensions/auto_role.py
+++ extensions/auto_role.py
@@ -29,13 +29,14 @@ class AutoRoles(Extension):
     # TODO:  if someone leaves check verification queue and remove if found.
     @listen()
     async def on_member_leave(self, event: MemberRemove):
-        player_in_queue = VerificationQueue.find(VerificationQueue.discord_id == event.member.id)
-        channel: ThreadChannel =  await self.bot.fetch_channel(player_in_queue.discord_thread)
-
-        if player_in_queue is not None:
-            logger.info(f"{event.member.name} has left the server, removing from verification queue.")
-            player_in_queue.delete()
-            await channel.delete()
+        # player_in_queue = VerificationQueue.find_one(VerificationQueue.discord_id == event.member.id)
+        # channel: ThreadChannel =  await self.bot.fetch_channel(player_in_queue.discord_thread)
+        async for playerInDb in VerificationQueue.find(VerificationQueue.discord_id == event.member.id):
+            thread_channel: ThreadChannel = await self.bot.fetch_channel(playerInDb.discord_thread)
+            if playerInDb is not None:
+                logger.info(f"{event.member.display_name} has left the server, removing from verification queue.")
+                await playerInDb.delete()
+                await thread_channel.delete()
 
 
 
